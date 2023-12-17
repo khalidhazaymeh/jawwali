a0c4b6dfc7acffc34c8d5c5d5b963144afa3e04f
khalid hazaymeh <149269856+khalidhazaymeh@users.noreply.github.com>
2023-12-17 12:06:05 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var wrapper= document.querySelector(".major-partner .wrapper");
+var carsouel= document.querySelector(".carousel");
+let isDragging=false , startX , startScrollLeft , timeoutId;
+const arrowBtns=document.querySelectorAll(".wrapper .arrow");
+
+//stats start
+
 let nums=document.querySelectorAll(".stats .number");
 let section=document.querySelector(".stats");
 let started=false;
@@ -18,171 +25,76 @@ function startcount(el){
 let goal=el.dataset.goal;
 let count =setInterval(() => {
     el.textContent++; 
-    if(el.textContent == goal){
+    if(el.textContent === goal){
         clearInterval(count);
 
     }
-}, 2000 / goal);
+}, 2500 / goal);
 };
+//stats end
 
+const firstCardWidth=carsouel.querySelector(".wrapper .carousel .card").offsetWidth;
+const carsouelChildrens=[...carsouel.children];
+let cardPreView = Math.round(carsouel.offsetWidth / carsouelChildrens);
 
+carsouelChildrens.slice(-cardPreView).reverse().forEach(card => {
+    carsouel.insertAdjacentHTML("afterbegin" , card.outerHTML);
+});
 
-//Get Slider Items | Array.from [ES6 Feature]
-var sliderImages=Array.from(document.querySelectorAll('.slider-container img'));
-//Get number of slider
-var slidesCount = sliderImages.length;
-//set current slide
-var currentSlide =1;
-//slider number string element
-var slideNumberElement=document.getElementById('slide-number');
+carsouelChildrens.slice(0,cardPreView).reverse().forEach(card => {
+    carsouel.insertAdjacentHTML("beforeend" , card.outerHTML);
+});
 
-//previous and next botton
-var nextBotton = document.getElementById('next');
-var prevBotton = document.getElementById('prev');
+const dragStart=(e)=>{
+    isDragging=true;
+    carsouel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carsouel.scrollLeft;
 
-//Handle Click on previous and next botton
-nextBotton.onclick=nextSlide;
-prevBotton.onclick=prevSlide;
-
-//create main ul element 
-var paginationElement = document.createElement('ul');
-//Set Id on created Element
-paginationElement.setAttribute('id','pagination-ul');
-
-//creat li based on array or slides count
-for(var i=1; i<=slidesCount;i++){
-// create li
-var paginationItem = document.createElement('li');
-//set custom Attribute 
-paginationItem.setAttribute('data-index',i);
-
-//set item content
-paginationItem.appendChild(document.createTextNode(i));
-//Append item to the main parent
-paginationElement.appendChild(paginationItem)
 }
 
-//add the ul element to the page 
-document.getElementById('indicators').appendChild(paginationElement);
+arrowBtns.forEach(btn =>{
+    btn.addEventListener("click" , ()=>{
+        carsouel.scrollLeft += btn.id === "icon-left" ? - firstCardWidth : firstCardWidth;
+    });
+});
+const dragging =(e)=>{
+    if(!isDragging) return;
+    carsouel.scrollLeft=startScrollLeft - (e.pageX - startX);
+};
+
+const dragStope = () =>{
+    isDragging=false;
+    carsouel.classList.remove("dragging");
+};
+const autoPlay= ()=>{
+    if(window.innerWidth < 800) return;
+    timeoutId=setTimeout(()=>carsouel.scrollLeft +=firstCardWidth,2500)
+};
+autoPlay();
 
 
-
-//get the new created ul 
-var paginationCreatedUl=document.getElementById('pagination-ul');
-
-//Get pagenation items | Array.from [ES6 Feature]
-var pagenationsBullets=Array.from(document.querySelectorAll('#pagination-ul li'));
-
-//loop through bullets items 
-for(var i =0; i<pagenationsBullets.length; i++){
-
-    pagenationsBullets[i].onclick=function(){
-        currentSlide=parseInt(this.getAttribute('data-index'));
-        theChecker ();  
+const infiniteScroll =()=>{
+    if(carsouel.scrollLeft === 0){
+        carsouel.classList.add("no-transition");
+        carsouel.scrollLeft = carsouel.scrollWidth - ( 10 * carsouel.offsetWidth);
+        carsouel.classList.remove("no-transition");
     }
-
-    
-}
-
-
-
-//tregger the checker function
-theChecker ();
-
-
-
-//function next slide
-
-function nextSlide(){
- if(currentSlide==slidesCount){ 
-    currentSlide=1;
-    theChecker ();
-    
- }else{
-    currentSlide++;
-    theChecker ();
-    
- }
-
-
-};
-function prevSlide(){
-    // if(prevBotton.classList.contains('disabled'))
-    if( currentSlide==1){
-        // return false;
-        currentSlide=slidesCount; 
-        theChecker ();
-        currentSlide=slidesCount;
-       
-
-    
-     }
-     else if(currentSlide==-1){
-        currentSlide=0;
-        theChecker ();
-     }
-     else{
-        currentSlide--;
-        theChecker ();
-     }
-
-
-
-     
-
-    
+    else if(Math.ceil(carsouel.scrollLeft) === carsouel.scrollWidth - carsouel.offsetWidth){
+        carsouel.classList.add("no-transition");
+        carsouel.scrollLeft = carsouel.offsetWidth;
+        carsouel.classList.remove("no-transition");
+    }
+clearTimeout(timeoutId);
+if(!wrapper.matches(":hover"))autoPlay();
 };
 
-
-//create checker function 
-// *********************************************************************************
-function theChecker (){
-    //set the slide number 
-    slideNumberElement.textContent='Slide # '+(currentSlide) + 'of ' + (slidesCount);
-// remove active slide from all images 
-removeAllActive();
- //set active slide on current slide 
-    sliderImages[currentSlide - 1].classList.add('active');
-  //set active class on current pagenation item
-paginationCreatedUl.children[currentSlide - 1].classList.add('active');
-//check if the current slide is the first
-if(currentSlide== -1){
-    
-    // //disable class on prev btn
-    // prevBotton.classList.add('disabled');
-    // currentSlide=slidesCount-1;
-    // paginationCreatedUl.children[currentSlide].classList.add('active');
-}else{
-     //remove disabled class on prev btn
-     prevBotton.classList.remove('disabled');
-};
-//check if the last one
-if(currentSlide==slidesCount){
-    //disable class on prev btn
-    // nextBotton.classList.add('disabled');    
-    currentSlide=0;
-    
-}else{
-     //remove disabled class on next btn
-     nextBotton.classList.remove('disabled');
-};
+carsouel.addEventListener("mousedown" , dragStart);
+carsouel.addEventListener("mousemove" , dragging);
+document.addEventListener("mouseup" , dragStope);
+carsouel.addEventListener("scroll" , infiniteScroll);
+wrapper.addEventListener("mouseenter" ,()=> clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave" , autoPlay);
 
 
-}
-// ********************************************************************************
 
-//remove all active clss from image and polites 
-function removeAllActive(){
-    //loop for on images 
-    sliderImages.forEach(function(img){
-        img.classList.remove('active')
-
-
-    });
-//loop for pagenation bullets
-    pagenationsBullets.forEach(function(bullet){
-        bullet.classList.remove('active')
-
-    });
-}
-// ********************************************************************************
